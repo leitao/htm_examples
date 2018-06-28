54e5fd951be2e6f98d60a24f4edaa0bed1d07083
Breno Leitao <leitao@debian.org>
2018-06-28 14:23:29 -0400

    Change from r1 to r0

--- signal_htm.c
+++ signal_htm.c
@@ -8,10 +8,11 @@ int htm(){
 	asm ("tbegin.  \n\t");
         asm goto ("beq %l[failure] \n\t" : : : : failure);
 	/* Load R0 with 0 */
-	asm ("li 1, 0 \n\t");
+	asm ("li 0, 0 \n\t");
 	asm ("trap \n\t");
 	asm ("tend. 0 \n\t");
 
+	return 0;
 failure:
 	printf("Failure handler called\n");
 	return 0;
@@ -24,22 +25,18 @@ void signal_handler(int signo, siginfo_t *si, void *data) {
 
 	mcontext_t *mcontext = (mcontext_t *) &uc->uc_mcontext;
 	printf("Checkpointed\n");
-	printf("R1 = %lx\n", mcontext->gp_regs[1]);
+	printf("R0 = %lx\n", mcontext->gp_regs[0]);
  
 	/* transactional machine context */
 	ucontext_t *tm_uc = uc->uc_link;
 	if (tm_uc != NULL){
 		mcontext_t *tm_mcontext = (mcontext_t *) &tm_uc->uc_mcontext;
-		if (tm_mcontext->gp_regs[1] != 0){
 			printf("Transactional different\n");
-			printf("R1 = %lx\n", tm_mcontext->gp_regs[1]);
-			exit(1);
-
-		}
+			printf("R0 = %lx\n", tm_mcontext->gp_regs[0]);
 	}
 
 	/* Exit the signal handler, if not transactional */
-	//mcontext->gp_regs[32] += 4; // skip illegal
+//	mcontext->gp_regs[32] += 12; // skip illegal
 }
 
 int main(){
