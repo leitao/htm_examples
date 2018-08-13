cad19a16eca8b0e6549af2de4f7bc66bce34d70c
Breno Leitao <leitao@debian.org>
2018-08-13 14:53:49 -0400

    Non HTM examples
    Signed-off-by: Breno Leitao <leitao@debian.org>

---
+++ nonhtm/extrdi/extrdi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int *argc, char **argv){
+	unsigned long res = -1; 
+	unsigned long rs = -1;
+
+	if (argv[1] != NULL)
+		rs = atol(argv[1]);
+	
+	printf("rs = %lx\n", rs);
+
+	asm __volatile__ (
+		" mr 4, %[rs] ;  \n"
+		" extrdi %[res], 4, 2, 29; \n"
+		: [res] "=r" (res)
+		: [rs] "r" (rs)
+		: "memory", "r4", "r3");
+
+	printf("1. res = %lx\n", res);
+
+	return 0;
+}
